5ab526e6b95156182e2a53a0ebd0740a11e46e9e
sergei sh <managerzf168@gmail.com>
2019-10-13 18:19:00 +0300

    working ant evolution

--- evolve_ant.cpp
+++ evolve_ant.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <cstdlib>
 #include <iostream>
+#include <functional>
 #include <random>
 #include <stdexcept>
 #include <string>
@@ -12,15 +13,16 @@
 
 using Individual = stree::gp::Individual;
 using Population = stree::gp::Population<Individual>;
+using Group = stree::gp::Group<Individual>;
 using Fitness = stree::gp::Fitness;
 
 static void usage(const std::string& name);
 static Trail load_trail_or_exit(const std::string& filename);
 
-static void evaluate(Individual& individual, const Trail& trail, unsigned step_limit);
+static Fitness evaluate(Individual& individual, const Trail& trail, unsigned step_limit);
 
-struct GetFitness {
-    GetFitness(Trail trail, unsigned step_limit)
+struct Evaluator {
+    Evaluator(Trail trail, unsigned step_limit)
         : trail(std::move(trail)),
           step_limit(step_limit) {}
 
@@ -31,6 +33,11 @@ struct GetFitness {
 };
 
 int main(int argc, char** argv) {
+    const unsigned GenerationMax = 20;
+    const Fitness FitnessGoal = 0.0;
+    const unsigned TournamentSize = 10;
+    const unsigned ResultNum = 10;
+
     /// Evaluation
     const unsigned StepLimit = 600;
 
@@ -41,7 +48,7 @@ int main(int argc, char** argv) {
 
     /// Crossover
     const unsigned CrossoverNum = 60;
-    const unsigned CrossoverOnePointPTerm = 0.1;
+    const unsigned CrossoverPTerm = 0.1;
 
     /// Mutation
     const unsigned MutationNum = 15;
@@ -70,8 +77,8 @@ int main(int argc, char** argv) {
         usage(argv[0]);
     Trail trail = load_trail_or_exit(argv[1]);
 
-    // Fitness getter
-    GetFitness get_fitness(trail, StepLimit);
+    // Evaluator
+    Evaluator evaluator(trail, StepLimit);
 
     // Random engine
     const unsigned PrngSeed = 1;
@@ -87,14 +94,113 @@ int main(int argc, char** argv) {
     env.add_select_function("is-food-ahead", 2, 0, &ant::forward);
 
     // Initialize population
+    unsigned generation = 0;
+    std::cout << "Generation 0" << std::endl;
     Population pop_current = stree::gp::ramped_half_and_half<Individual>(
         env, PopulationSize, InitMaxDepth, InitPTermGrow, prng);
-    // TEST
-    for (Individual& individual : pop_current) {
-        std::cout << individual.tree() << std::endl
-                  << get_fitness(individual) << std::endl
-                  << std::endl;
-    }
+
+    bool done = false;
+    do {
+        // Reap results
+        Group best = stree::gp::reap<Individual>(pop_current, ResultNum, evaluator);
+        done = stree::gp::is_goal_achieved<Individual>(best, FitnessGoal, evaluator)
+            || generation == GenerationMax;
+        if (done) {
+            std::cout << "Best results" << std::endl;
+            for (auto item : best) {
+                Individual& individual = item.get();
+                std::cout << "[" << individual.fitness() << "] "
+                          << individual.tree()
+                          << std::endl;
+            }
+        } else {
+            assert(best.size() > 0);
+            std::cout << "Best fitness: ";
+            for (auto item : best) {
+                Individual& individual = item.get();
+                std::cout << individual.fitness() << " ";
+            }
+            std::cout << std::endl;
+        }
+
+        if (!done) {
+            ++generation;
+            std::cout << std::endl;
+            std::cout << "Generation " << generation << std::endl;
+            Population pop_next;
+            unsigned index = 0, max_index = 0;
+
+            /// Crossover
+            max_index += CrossoverNum;
+            for (; index < max_index; ++index) {
+                // Select parents
+                Individual& parent1 = stree::gp::selection_tournament<Individual>(
+                    pop_current, TournamentSize, prng, evaluator);
+                Individual& parent2 = stree::gp::selection_tournament<Individual>(
+                    pop_current, TournamentSize, prng, evaluator);
+                // Apply crossover
+                // stree::Tree child = stree::gp::crossover_one_point(
+                //     parent1.tree(),
+                //     parent2.tree(),
+                //     CrossoverPTerm,
+                //     prng);
+                stree::Tree child = stree::gp::crossover_random(
+                    parent1.tree(),
+                    parent2.tree(),
+                    CrossoverPTerm,
+                    prng);
+                // Add child
+                pop_next.emplace_back(std::move(child));
+            }
+
+            /// Mutation
+            // Random subtree mutation
+            max_index += MutationSubtreeNum;
+            for (; index < max_index; ++index) {
+                Individual& individual = stree::gp::selection_tournament<Individual>(
+                    pop_current, TournamentSize, prng, evaluator);
+                pop_next.emplace_back(
+                    stree::gp::mutate_subtree(
+                        individual.tree(),
+                        MutationSubtreeDepth,
+                        MutationSubtreePTerm,
+                        MutationSubtreePTermGrow,
+                        prng));
+            }
+            // Point mutation
+            max_index += MutationPointNum;
+            for (; index < max_index; ++index) {
+                Individual& individual = stree::gp::selection_tournament<Individual>(
+                    pop_current, TournamentSize, prng, evaluator);
+                pop_next.emplace_back(
+                    stree::gp::mutate_point(
+                        individual.tree(),
+                        MutationPointPTerm,
+                        prng));
+            }
+            // Hoist mutation
+            max_index += MutationHoistNum;
+            for (; index < max_index; ++index) {
+                Individual& individual = stree::gp::selection_tournament<Individual>(
+                    pop_current, TournamentSize, prng, evaluator);
+                pop_next.emplace_back(
+                    stree::gp::mutate_hoist(
+                        individual.tree(),
+                        MutationHoistPTerm,
+                        prng));
+            }
+
+            /// Reproduction
+            while (pop_next.size() < pop_current.size()) {
+                Individual& individual = stree::gp::selection_tournament<Individual>(
+                    pop_current, TournamentSize, prng, evaluator);
+                pop_next.emplace_back(individual.tree().sub(0).copy());
+            }
+
+            /// Swap populations
+            pop_current.swap(pop_next);
+        }
+    } while(!done);
 
     return 0;
 }
@@ -120,7 +226,7 @@ Trail load_trail_or_exit(const std::string& filename) {
     assert(false);
 }
 
-void evaluate(Individual& individual, const Trail& trail, unsigned step_limit) {
+Fitness evaluate(Individual& individual, const Trail& trail, unsigned step_limit) {
     // Make ant
     Ant ant(trail);
     // Run program
@@ -131,12 +237,9 @@ void evaluate(Individual& individual, const Trail& trail, unsigned step_limit) {
     while (ant.food_left() > 0 && step++ <= step_limit)
         exec.step();
     // Set individual fitness
-    Fitness fitness = static_cast<float>(ant.food_left()) / trail.size();
-    individual.set_fitness(fitness);
+    return static_cast<float>(ant.food_left()) / trail.size();
 }
 
-Fitness GetFitness::operator()(Individual& individual) {
-    if (!individual.fitness())
-        evaluate(individual, trail, step_limit);
-    return individual.fitness();
+Fitness Evaluator::operator()(Individual& individual) {
+    return evaluate(individual, trail, step_limit);
 }
