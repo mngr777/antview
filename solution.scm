630ff810d3a5caa8bcc05898ea0b51a5fed070e7
sergei sh <sshoulbakov@kontur.io>
2022-03-12 22:50:42 +0300

    solution formatted

--- solution.scm
+++ solution.scm
@@ -1,1 +1,15 @@
-(if-food-ahead (progn3 (forward) (forward) (right)) (progn2 (progn2 (progn3 (left) (forward) (left)) (if-food-ahead (forward) (right))) (right)))
+(if-food-ahead
+ (progn3
+  (forward)
+  (forward)
+  (right))
+ (progn2
+  (progn2
+   (progn3
+    (left)
+    (forward)
+    (left))
+   (if-food-ahead
+    (forward)
+    (right)))
+  (right)))
